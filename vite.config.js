c4261debcc4a7a0c699d556c109810d062a8c94e
04Sai <ic.rexsymond.barba@cvsu.edu.ph>
2025-05-09 15:41:21 +0800

    feat: Add Netlify configuration and update build scripts; include ToDoIcon in .gitignore

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+    plugins: [react(), tailwindcss()],
+    server: {
+        port: 3005,
+    },
+    base: './', // Add this line to make deployed assets use relative paths
+})
